4b847dfb2f7242f9763f0801fe67bc3400f52d96
Rason <Dummy>
2019-06-18 00:27:28 +0500

    my emacs dots

---
+++ .emacs.d/elpa/jedi-20160426.456/jedi-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "jedi" "20160426.456" "a Python auto-completion for Emacs" '((emacs "24") (jedi-core "0.2.2") (auto-complete "1.4")) :commit "615544c6ca81bbc53140aefe345e2120110c1660" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>"))
